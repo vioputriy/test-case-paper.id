fd99d96464d6fb05593c7af5fa83eab802dfe637
vio putri yuwandika <46666598+vioputriy@users.noreply.github.com>
2024-06-04 21:46:21 +0700

    Update paper.js

--- paper.js
+++ paper.js
@@ -1,10 +1,18 @@
-describe('', function(){
-    it('tc-1',function(){
-       cy.visit('https://www.paper.id/webappv1/#/login')
-    })
-})
+describe('login', () => {
+  it('passes', () => {
+    cy.visit('https://www.paper.id/webappv1/#/login')
+    cy.get('[data-cy="identifier"]').type('kandidat@paper.id')
+    cy.get('[data-cy="submit"]').click()
+    cy.get('[data-cy="password"]').type('paper.id')
+    cy.get('.auth-input-icon').click()
+    cy.get('.login-password-dialog__password > form.ng-dirty > [data-cy="submit"]').click();
 
-
-Cypress.on('uncaught:exception', (err, runnable)=>{
-    return false
+  })
 })
+describe('unregister', () => {
+ it('passes', () => {
+    cy.visit('https://www.paper.id/webappv1/#/login')
+    cy.get('[data-cy="identifier"]').type('vioputriyuwan@gmail.com')
+    cy.get('[data-cy="submit"]').click()
+   })
+ })
